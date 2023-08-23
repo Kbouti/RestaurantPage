baef58137f41c2f2ec55e232690eb854586ac196
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-23 16:05:19 -0700

    Write test function and create webpack file

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{let o=prompt("What is your name?");console.log(`Hello ${o}, how can I serve you today?`)})();
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const logSomething = () =>{
+    let name = prompt(`What is your name?`);
+
+    console.log(`Hello ${name}, how can I serve you today?`)
+}
+
+logSomething();
