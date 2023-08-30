96e0138fc664c6101f162a9cee93c652a4daf286
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-30 12:49:13 -0700

    Edit packege.json

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 console.log(`Welcome to the console of the pizza website of the Kevdawg`);
 
 
-function sayHello() {
-    console.log(`Hello Kevin`);
-}
-
-
-sayHello()
-
-
 function addElement() {
     let element = document.createElement(`div`);
-    element.innerHTML = (`Hi Kevin`)
+    element.innerHTML = (`Hi Kevin< i'm a div made by Javascript!`)
 
     return element;
 }
