5ee81115be3659fdfe60227bf094a4f0b2fd7d87
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-30 15:48:23 -0700

    Append child to content instead of body

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import _ from 'lodash';
 console.log(`Welcome to the console of the pizza website of the Kevdawg`);
 
 
+
+// loadPage()
+
+
+const content = document.getElementById(`content`);
+
 function addElement() {
     let element = document.createElement(`div`);
     element.innerHTML = (`Hi Kevin, i'm a div made by Javascript!`);
@@ -11,4 +17,4 @@ function addElement() {
     return element;
 }
 
-document.body.appendChild(addElement());
+content.appendChild(addElement());
