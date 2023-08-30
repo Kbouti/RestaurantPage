5cb7fb11ea7e27d26e26f68ece901aaa3bf22519
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-30 15:10:47 -0700

    Create webpack configuration file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ console.log(`Welcome to the console of the pizza website of the Kevdawg`);
 
 function addElement() {
     let element = document.createElement(`div`);
-    element.innerHTML = (`Hi Kevin< i'm a div made by Javascript!`)
+    element.innerHTML = (`Hi Kevin, i'm a div made by Javascript!`);
+    element.className = "test";
 
     return element;
 }
