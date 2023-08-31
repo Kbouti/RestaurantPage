2c80b9a255d940ab375a716239bb21b3a3318d49
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-31 10:54:26 -0700

    Create header items with javascript

--- src/homepage.js
+++ src/homepage.js
@@ -7,19 +7,31 @@ const homepage = () => {
 
     const content = document.getElementById(`content`);
 
+    const mainContain = document.createElement(`div`);
+    mainContain.setAttribute(`id`, `mainContain`);
 
-    function addMainContain() {
-        let mainContain = document.createElement(`div`);
-        mainContain.setAttribute(`id`, `mainContain`);
-        return mainContain;
-    }
-
-    
+    const mainHeader = document.createElement(`div`);
+    mainHeader.setAttribute(`id`, `mainHeader`);
 
 
+    const headerItem1 = document.createElement('div');
+    headerItem1.classList.add(`headerItem`);
+    headerItem1.innerHTML = '215-327-1557'
 
-    content.appendChild(addMainContain());
 
+    const headerItem2 = document.createElement('div');
+    headerItem2.setAttribute(`id`, `headerLogo`)
+
+    const headerItem3 = document.createElement('div');
+    headerItem3.classList.add(`headerItem`);
+    headerItem3.innerHTML = 'Order Now!'
+
+
+    mainContain.appendChild(mainHeader);
+    content.appendChild(mainContain);
+    mainHeader.appendChild(headerItem1);
+    mainHeader.appendChild(headerItem2);
+    mainHeader.appendChild(headerItem3);
 
 }
 
