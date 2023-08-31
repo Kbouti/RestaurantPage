da7faf62ba16779328b1d62072adbe631de54df4
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-08-31 11:50:02 -0700

    Complete mainheader with image

--- src/homepage.js
+++ src/homepage.js
@@ -9,28 +9,41 @@ const homepage = () => {
 
     const mainContain = document.createElement(`div`);
     mainContain.setAttribute(`id`, `mainContain`);
+    content.appendChild(mainContain);
+
+
 
     const mainHeader = document.createElement(`div`);
     mainHeader.setAttribute(`id`, `mainHeader`);
+    mainContain.appendChild(mainHeader);
+
+    const subHeader = document.createElement(`div`);
+    subHeader.setAttribute(`id`, `subHeader`);
+    mainContain.appendChild(subHeader);
+
+
 
 
     const headerItem1 = document.createElement('div');
     headerItem1.classList.add(`headerItem`);
-    headerItem1.innerHTML = '215-327-1557'
+    headerItem1.innerHTML = '215-327-1557';
+    mainHeader.appendChild(headerItem1);
+
 
 
     const headerItem2 = document.createElement('div');
-    headerItem2.setAttribute(`id`, `headerLogo`)
+    // headerItem2.classList.add(`headerItem`);
+    headerItem2.setAttribute(`id`, `headerLogo`);
+    const logoImage = document.createElement('img');
+    logoImage.src = "/src/images/kevdogPizzaLogo.png";
+    logoImage.alt = "Pizza By Kevdog";
+    headerItem2.appendChild(logoImage);
+    mainHeader.appendChild(headerItem2);
+    
 
     const headerItem3 = document.createElement('div');
     headerItem3.classList.add(`headerItem`);
-    headerItem3.innerHTML = 'Order Now!'
-
-
-    mainContain.appendChild(mainHeader);
-    content.appendChild(mainContain);
-    mainHeader.appendChild(headerItem1);
-    mainHeader.appendChild(headerItem2);
+    headerItem3.innerHTML = 'Order Now!';
     mainHeader.appendChild(headerItem3);
 
 }
