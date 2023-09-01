4d0b6f48cb74ea079fca66103ea13c3b07c0e5c6
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-09-01 08:23:25 -0700

    Remove body elements on menu button click

--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,18 @@ const addMenuListener = () => {
     const menuBtn = document.getElementById(`menuBtn`);
     menuBtn.addEventListener(`click`, ()=>{
         console.log(`You clicked the menu button`);
+
+        let mainContent = document.getElementById(`mainContent`);
+        let body = document.getElementById(`body`);
+
+        body.removeChild(mainContent);
+        body.removeChild(review1);
+        body.removeChild(review2);
+
+
+
+        
+
     })
 }
 
