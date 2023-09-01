7ec50d0ea1bd8d085e2c56309a98df53a6be0631
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-09-01 16:41:47 -0700

    Update webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,15 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    modules: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    `style-loader`,
+                    `css-loader`,
+                ],
+            },
+        ],
+    },
 };
