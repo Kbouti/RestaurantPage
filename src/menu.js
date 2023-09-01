bb93138220f4364cb485dc12b730a24884b75fce
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-09-01 08:49:43 -0700

    Add menu title

--- src/menu.js
+++ src/menu.js
@@ -10,6 +10,14 @@ const addMenuListener = () => {
         body.removeChild(review1);
         body.removeChild(review2);
 
+        const bodyMenu = document.createElement(`div`);
+            bodyMenu.setAttribute(`id`, `bodyMenu`);
+            body.appendChild(bodyMenu);
+
+        const menuTitle = document.createElement(`div`);
+            menuTitle.setAttribute(`id`, `menuTitle`);
+            menuTitle.innerHTML = `Menu`;
+            bodyMenu.appendChild(menuTitle);
 
 
         
