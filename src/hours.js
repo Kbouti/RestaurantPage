748c9037857ab4f7f2d942bc373f2e0b2b870eb4
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-09-04 10:41:26 -0700

    Add and edit store hours

--- src/hours.js
+++ src/hours.js
@@ -22,6 +22,9 @@ const addHoursListener = () => {
             hoursContent.setAttribute(`id`, `hoursContent`);
             hoursContent.setAttribute(`class`, `bodySubtext`);
 
+
+
+            
             const mon = document.createElement(`div`);
             const tue = document.createElement(`div`);
             const wed = document.createElement(`div`);
@@ -30,7 +33,30 @@ const addHoursListener = () => {
             const sat = document.createElement(`div`);
             const sun = document.createElement(`div`);
 
-            const days = [];
+            mon.innerHTML = "Monday: 11am - 9pm";
+            tue.innerHTML = "Tuesday: 11am - 9pm";
+            wed.innerHTML = "Wednesday: 11am - 9pm";
+            thu.innerHTML = "Thursday: 11am - 9pm";
+            fri.innerHTML = "Friday: 11am - 11pm";
+            sat.innerHTML = "Saturday: 9am - 11pm";
+            sun.innerHTML = "Sunday: 9am - 11pm";
+
+            mon.classList.add(`dayHour`);
+            tue.classList.add(`dayHour`);
+            wed.classList.add(`dayHour`);
+            thu.classList.add(`dayHour`);
+            fri.classList.add(`dayHour`);
+            sat.classList.add(`dayHour`);
+            sun.classList.add(`dayHour`);
+
+        hoursContent.appendChild(mon);
+        hoursContent.appendChild(tue);
+        hoursContent.appendChild(wed);
+        hoursContent.appendChild(thu);
+        hoursContent.appendChild(fri);
+        hoursContent.appendChild(sat);
+        hoursContent.appendChild(sun);
+
 
             bodyHours.appendChild(hoursContent);
 
