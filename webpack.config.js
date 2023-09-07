5ce3a1b12adf83cdb19a689f15f5fe8047793f1b
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-09-07 14:26:20 -0700

    Begin formatting question in webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -22,3 +22,12 @@ module.exports = {
         ],
     },
 };
+
+
+
+// I'm working on the restaurant page project and I'm struggling to publish my project on github. 
+// When accessed through dist/index.html my site works fine and has css and images. 
+
+// When accessing from github the images do not load. The "alt" text appears instead of the image.  I have the necessary css and image loaders in my webpack.config.js file.
+// 
+
